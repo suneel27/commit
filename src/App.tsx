ddc0c3519a296aaf8f5471d64c633769ab2f18c4
Vukoti Suneel Kumar <153293027+suneel27@users.noreply.github.com>
2026-04-01 21:48:05 +0530

    Create src/App.tsx with React GitHub Calendar

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Calendar } from 'react-github-calendar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Portfolio of Suneel27</h1>
+      <h2>Full-Stack Developer</h2>
+      <Calendar username="suneel27" />
+    </div>
+  );
+}
+
+export default App;
